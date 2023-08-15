add7a1c20f02833e2f91faa98d39bea4521ca3a6
Akash Dholakiya <akashdholakiya222@gmail.com>
2023-08-15 18:22:48 +0530

    add this

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
 import './App.css';
+import Alert from './components/Alert';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React,{useState} from 'react'
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from  "react-router-dom";
 
 function App() {
+  const [mode,setMode] = useState('light');  
+  const [alert,setAlert] = useState(null);
+
+  const showAlert = (message,type) => {
+      setAlert({
+        msg : message,
+        type : type
+      })
+      setTimeout(() => {
+          setAlert(null);
+      }, 2000);
+  }
+
+  const btnChange = () => {
+    if(mode === "dark"){
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert('Light Mode has been enabled',"success");
+      document.title = "TextUtils - Light Mode";
+    }else{
+      setMode('dark');
+      document.body.style.backgroundColor = 'silver';
+      showAlert('Dark Mode has been enabled',"success");
+      document.title = "TextUtils - Dark Mode";
+    }
+  }
   return (
     <>
-    <Navbar title="TextUtils" aboutText ="About TextUtils"/>
+    <Router>
+    <Navbar title="TextUtils" aboutText ="About TextUtils" mode={mode} btnChange={btnChange} />
+    <Alert alert={alert}/>
     <div className="container my-3">
-      <TextForm heading = "Enter the heading here"/>
+      <Routes>
+        <Route path='/about' element={<About />} />
+        <Route path='/' element={<TextForm heading = "Enter the heading here" mode={mode} showAlert={showAlert}/>} />
+      </Routes>
     </div>
+    </Router>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,28 +2,52 @@ import React, {useState} from 'react'
 
 
 export default function TextForm(props) {
+    const fun1 = (word) => {
+        let c = 0;
+        for(let i = 0; i < word.length; i++){
+            if(word[i] !== ' ')c++;
+        }
+        return c;
+    }
+    const fun2 = (words) => {
+        let c = 0;
+        if(words.length === 0)return 0;
+        for (let i = 0; i < words.length-1; i++) {
+            if(words[i] === ' ' && words[i+1] !== ' ')c++;
+        }
+        return c+1;
+    }
     const handleUpper = () => {
-        // setText("you have clicked handleUpper" + text);
         let newText = text.toUpperCase();
         setText(newText);
+        props.showAlert('Text Converted to Upper case','success');
+    }
+    const clipit = () => {
+        let txt = document.getElementById('clip-box');
+        txt.select();
+        txt.setSelectionRange(0,9999);
+        navigator.clipboard.writeText(txt.value);
+        props.showAlert('Copied to ClipBoard','success');
     }
     const changeHand = (event) => {
         setText(event.target.value);
     } 
     const [text,setText] = useState('');  
+    
   return (
     <>
-    <div className='container'>
+    <div className='container' >
         <h1>{props.heading}</h1>
         <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={changeHand} id="exampleFormControlTextarea1" rows="8"></textarea>
+            <textarea className="form-control" value={text} onChange={changeHand} id="clip-box" rows="8"></textarea>
         </div>
-        <button onClick={handleUpper} className="btn btn-primary">Change to UpperCase</button>
+        <button onClick={handleUpper} className="btn btn-primary mx-2">Change to UpperCase</button>
+        <button onClick={clipit} className="btn btn-primary" >Copy to Clipboard</button>
     </div>
     <div className="container my-3">
         <h1>Your Text Summary</h1>
-        <p>{text.split(" ").length} words and {text.length  } charactors</p>
-        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <p>{fun2(text)} words and {fun1(text)} charactors</p>
+        <p>{0.008 * fun2(text)} Minutes read</p>
         <h2>Preview</h2>
         <p>{text}</p>
     </div>
